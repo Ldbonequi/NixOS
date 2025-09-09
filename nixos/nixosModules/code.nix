14e2764076f028bb46d9f17b3b645f8db860b1d4
ldbonequi <ldbonequi@gmail.com>
2025-09-08 20:10:15 -0600

    add auto pep8 and descriptions

--- nixos/nixosModules/code.nix
+++ nixos/nixosModules/code.nix
@@ -15,9 +15,10 @@
     jdk23 #java stable 23
     gradle #java build system
     (python3.withPackages(p: with p; [ #python stable build with packages below use instead of pip
-      numpy
-      pandas
-      pycodestyle
+      numpy #math
+      pandas #math / linear algebra stuff
+      pycodestyle #detect pep 8 errors in python
+      autopep8 #auto python formater to pep8 standard
     ])) 
     gpclient #csu connection vpn
       globalprotect-openconnect #gui for gpclient
