df36d767740a5f3aaeda21c5d47f396da3597fc9
ldbonequi <ldbonequi@gmail.com>
2025-09-28 15:50:14 -0600

    fix maven capitalization

--- nixos/nixosModules/code.nix
+++ nixos/nixosModules/code.nix
@@ -14,7 +14,7 @@
     	wl-clipboard #allow nvim to access system keyboard
     jdk23 #java stable 23
       gradle #java build system
-      Maven #Build automation tool for java
+      maven #Build automation tool for java
     (python3.withPackages(p: with p; [ #python stable build with packages below use instead of pip
       numpy #math
       pandas #math / linear algebra stuff
